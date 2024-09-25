3b094af13a5c76c82ceb2faceefc2c545b243904
syedm83 <syed_m8@yahoo.com>
2024-09-25 09:45:54 -0400

    added category stuff

---
+++ inventory/categories.sql
@@ -0,0 +1,14 @@
+CREATE TABLE categories (
+ categoryID       INT(11)        NOT NULL,
+ categoryCode     VARCHAR(10)    NOT NULL,
+ categoryName     VARCHAR(255)   NOT NULL,
+ PRIMARY KEY (categoryID)
+);
+
+INSERT INTO categories
+(categoryID, categoryCode, categoryName)
+VALUES
+(100, 'GRTS', 'Guitars');
+DELETE from categories where categoryID = 100;
+
+SELECT * FROM categories;
